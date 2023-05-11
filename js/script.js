614c8f69140f1edd0cac7b86400ebcf3c4338750
DevMiks1 <MikeAlfredVargas1@gmail.com>
2023-05-11 21:49:49 +0800

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// hero section
+
+
+ScrollReveal({ reset: false, distance: "60px", duration: 2500, delay:500});
+ScrollReveal().reveal('.profile-name', { delay: 500, origin: "left" });
+ScrollReveal().reveal('.social', { delay: 600, origin: "bottom" });
+ScrollReveal().reveal('.right-hero-section', { delay: 700, origin: "right"});
+// know me section
+ScrollReveal().reveal('.know-me-image', { delay: 500, origin: "left"});
+ScrollReveal().reveal('.know-me-description', { delay: 600, origin: "right"});
+ScrollReveal().reveal('.scroll-btn', { delay: 700, origin: "bottom"});
+
+// blogs section
+ScrollReveal().reveal('.blogs-image', { delay: 400, origin: "right"});
+ScrollReveal().reveal('.blogs-description', { delay: 500, origin: "left"});
+ScrollReveal().reveal('.scroll-btn1', { delay: 550, origin: "bottom"});
+
+// services section
+ScrollReveal().reveal('.title-services', { delay: 550, origin: "bottom"});
+ScrollReveal().reveal('.card', { delay: 600, origin: "bottom"});
